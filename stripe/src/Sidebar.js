4d91fef386028284eee4cdccbf3e9b1fef7aa796
Nate Traugh <n.traugh@yahoo.com>
2022-08-24 18:52:09 -0400

    sidebar shows on mobile screens but not on larger screens

--- stripe/src/Sidebar.js
+++ stripe/src/Sidebar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import sublinks from './data'
+import { useGlobalContext } from './context'
+import {FaTimes} from "react-icons/fa"
 
 const Sidebar = () => {
+  const {sidebarOpen, closeSidebar} = useGlobalContext()
   return (
-    <div>Sidebar</div>
+    <aside className={`${sidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"}`}>
+        <div className='sidebar'>
+          <button className='close-btn' onClick={closeSidebar}>
+            <FaTimes /> 
+          </button>
+        </div>
+    </aside>
   )
 }
 
