de96f102873424205f01f99841fb74440795cce2
Nate Traugh <n.traugh@yahoo.com>
2022-08-24 17:48:25 -0400

    set up our basic context.js with appProvider and functions to open and close sidebar/Modal components

--- stripe/src/context.js
+++ stripe/src/context.js
@@ -1,2 +1,27 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+// ALWAYS DESTRUCTURE CHILDREN
+const appProvider = ({children}) => {
+    const [modalOpen, setModalOpen] = useState(false)
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
+    const openSidebar = () => {
+        setSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setSidebarOpen(false)
+    }
+    const openModal = () => {
+        setModalOpen(true)
+    }
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+
+    <AppContext.Provider value={}>
+        {children}
+    </AppContext.Provider>
+}
