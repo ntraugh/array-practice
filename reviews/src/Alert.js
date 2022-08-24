8a23f14e218a707212fba92e7a1d5e718e945add
Nate Traugh <n.traugh@yahoo.com>
2022-08-24 14:33:13 -0400

    created useEffect hook to hide the alert after 3 seconds on screen

--- reviews/src/Alert.js
+++ reviews/src/Alert.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-// destructuring the alert state value here, don't need show. 
-const Alert = ({alert}) => {
+// grabbing alert prop from the App component. 
+const Alert = ({alert, unShowAlert}) => {
+
+  // using useEffect to unShow the alert after 3000 ms or 3 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+        unShowAlert()
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [])
   return (
     // setup a classname with alert property and then a second css property that is dynamic depending on the state
     <p className={`alert alert-${alert.type}`}>{alert.msg}</p>
--- reviews/src/App.js
+++ reviews/src/App.js
@@ -8,13 +8,13 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({ show: true, msg: "Alert", type: "success"})
+  const [alert, setAlert] = useState({ show: false, msg: "Alert", type: "success"})
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if(!name) {
-      // display the alert if there is no info in the search
-
+      // call the showAlert function that runs the setAlert function
+      showAlert(true, "danger", "Enter Some Input")
     } else if (name && isEditing){
         // if there is something in the input value AND isEditing state is true
 
@@ -28,11 +28,16 @@ function App() {
     }
   }
 
+  // function that takes in show, type, and msg which we give default values, then runs the setAlert function to set the values for the parameters
+  const showAlert = (show=false, type="", msg="") => {
+      setAlert({show, type, msg})
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {/* this conditional is checking our state of alert with the property of show. If it's true we show the alert */}
-        {alert.show && <Alert alert={alert}/>}
+        {alert.show && <Alert alert={alert} unShowAlert={showAlert}/>}
         <h3>Grocery Helper</h3>
         <div className="form-control">
           <input 
