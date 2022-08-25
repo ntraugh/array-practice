f487d8a07835226ace32db306e46fc2a30dea667
Nate Traugh <n.traugh@yahoo.com>
2022-08-25 15:05:10 -0400

    changed the value passed into the AppContext.Provider to {...state} which passes loading,cart,total,amount to the components that use it, for now since CartContainer has access to cart through useGlobalContext our data shows up

--- cart/src/context.js
+++ cart/src/context.js
@@ -7,11 +7,18 @@ const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const AppContext = React.createContext()
 
+const initialState = {
+    loading:false,
+    cart:cartItems,
+    total:0,
+    amount:0
+}
+
 const AppProvider = ({ children }) => {
-    const [cart, setCart] = useState(cartItems)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <AppContext.Provider value={ { cart, } }
+        <AppContext.Provider value={ { ...state } }
         >
             {children}
         </AppContext.Provider>
---
+++ cart/src/reducer.js
@@ -0,0 +1,7 @@
+
+// state is our initial state and action is what we want to do to it
+const reducer = (state, action) => {
+    return state
+}
+
+export default reducer
