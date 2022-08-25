e4559cea1d9568a699211bbad1981f6b5f0179f1
Nate Traugh <n.traugh@yahoo.com>
2022-08-25 15:58:10 -0400

    added a filter after the map to check if the amount is zero and just remove the item if that is the case

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -6,6 +6,23 @@ const reducer = (state, action) => {
     } else if (action.type === "REMOVE") {
         // grab everything in state, then grab the cart but only where the id's don't match the id we passed in (action.payload is the items id)
         return { ...state, cart:state.cart.filter((item) => item.id !== action.payload)}
+    } else if (action.type === "INCREASE") {
+        let tempCart = state.cart.map((item) => {
+            if(item.id === action.payload) {
+                // if the item id matches the payload id we passed in then we copy the whole item again and change it's amount by adding 1 
+                return { ...item, amount: item.amount + 1}
+            }
+            return item
+        })
+        return { ...state, cart: tempCart}
+    } else if (action.type === "DECREASE") {
+        let tempCart = state.cart.map((item) => {
+            if(item.id === action.payload) {
+                return { ...item, amount: item.amount - 1}
+            }
+            return item
+        }).filter((cartItem) => cartItem.amount !== 0 )
+        return {...state, cart: tempCart}
     }
     return state
 }
