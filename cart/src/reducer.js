f00e78d9bd083131e9edbb04de75a810bdf3ba53
Nate Traugh <n.traugh@yahoo.com>
2022-08-25 15:30:38 -0400

    used reducer function to clear cart by checking if action.type matches CLEAR_CART and if so we return the whole state value and cart with a new value of an empty array {...state, cart:[]}

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -1,6 +1,9 @@
 
 // state is our initial state and action is what we want to do to it
 const reducer = (state, action) => {
+    if(action.type === "CLEAR_CART") {
+        return { ...state, cart: []}
+    }
     return state
 }
 
