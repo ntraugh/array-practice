173c4bab16a3c1b9ab4a857ff0e77922958b77e3
Nate Traugh <n.traugh@yahoo.com>
2022-08-25 12:39:34 -0400

    created sublinks sidebar menu when screen is smaller than 800px. mapped over sublinks and destructured links(array of objects) and page to display page. then mapped over our links since it is also an array to display our labels and icons

--- stripe/src/Sidebar.js
+++ stripe/src/Sidebar.js
@@ -11,6 +11,29 @@ const Sidebar = () => {
           <button className='close-btn' onClick={closeSidebar}>
             <FaTimes /> 
           </button>
+          {/* we want the index from this map becauase we are making lists */}
+          <div className='sidebar-links'>
+            {sublinks.map((link, index) => {
+              // we destructure the link and page from our data right away
+              const {links, page} = link
+              return <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {/* we can map over links because it is also an array nested in our original array */}
+                  {links.map((link, index) => {
+                    // destructure our links right away
+                    const {label, icon, url} = link
+                    return <a key={index} href={url}>
+                      {icon}
+                      {label}
+                    </a>
+                      
+                    
+                  })}
+                </div>
+              </article>
+            })}
+          </div>
         </div>
     </aside>
   )
