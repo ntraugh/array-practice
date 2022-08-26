f5ee155ac3b53da81aa5f803eef559ba265ef6ea
Nate Traugh <n.traugh@yahoo.com>
2022-08-26 12:46:50 -0400

    using useParams we grab the id from our page that corresponds with the link they clicked on in the Cocktail Component

--- cocktails/src/pages/SingleCocktail.js
+++ cocktails/src/pages/SingleCocktail.js
@@ -4,8 +4,10 @@ import { useParams, Link } from "react-router-dom"
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const { id } = useParams()
+
   return (
-    <div>Cocktail</div>
+    <div>{id}</div>
   )
 }
 
