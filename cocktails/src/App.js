0624fca6fe0a150911f598878bed7ebe7a03a1ad
Nate Traugh <n.traugh@yahoo.com>
2022-08-26 11:02:42 -0400

    setup react router in App.js and put BrowserRouter in the index.js still inside of our AppProvider

--- cocktails/src/App.js
+++ cocktails/src/App.js
@@ -1,17 +1,25 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import SingleCocktail from "./pages/SingleCocktail"
 import Error from "./pages/Error"
 import About from "./pages/About"
-
+import Navbar from "./components/Navbar"
 
 
 function App() {
   return (
     <>
-      <Home />
-      <SingleCocktail />
-      <Error />
-      <About />
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          
+          <Route path="/about" element={<About />} />
+          
+          <Route path="/cocktail/:id" element={<SingleCocktail />} />
+          
+          <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
