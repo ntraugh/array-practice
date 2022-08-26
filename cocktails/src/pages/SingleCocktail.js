803bff6774e5a08d4f15db1490661f9785c128c5
Nate Traugh <n.traugh@yahoo.com>
2022-08-26 12:53:00 -0400

    getting back the specific drink that matches the ID in the console, need to display it on the page now in the return

--- cocktails/src/pages/SingleCocktail.js
+++ cocktails/src/pages/SingleCocktail.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Loading from "../components/Loading"
 import { useParams, Link } from "react-router-dom"
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  // useParams gives us back the number in our parameter from our URL. since we set up each single page to be /cocktail/:id it is grabbing that specific ID
   const { id } = useParams()
+  const [ loading, setLoading] = useState(false)
+  const [cocktail, setCocktail] = useState(null)
+  
+  useEffect(() => {
+    setLoading(true)
+    const getCocktail = async () => {
+        try {
+          const res = await fetch(`${url}${id}`)
+          const data = await res.json()
+          console.log(data)
+        } catch (err) {
+          console.log(err)
+        }
+    }
+    getCocktail()
+  }, [id])
 
   return (
     <div>{id}</div>
