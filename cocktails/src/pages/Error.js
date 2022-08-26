d15373c8bfc70cfdf546dc978651bd874b7d1c22
Nate Traugh <n.traugh@yahoo.com>
2022-08-26 11:24:15 -0400

    created Error page with link back to home

--- cocktails/src/pages/Error.js
+++ cocktails/src/pages/Error.js
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom"
 
 const Error = () => {
   return (
-    <h2>Error</h2>
+    <section className='error-page section'>
+      <div className='error-container'>
+        <h2>Dead end</h2>
+        <Link to="/" className='btn btn-primary'>
+          Back to Home
+        </Link>
+      </div>
+    </section>
   )
 }
 
