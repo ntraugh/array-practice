ae160fff657f886e1d91d942b92d828e5b2865d2
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 11:31:19 -0400

    using Row and Col from antdesign are very much like bootstrap, Col's take up 24 total width though instead of 12 in bootstrap

--- cryptoapp/src/components/Homepage.js
+++ cryptoapp/src/components/Homepage.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import millify from "millify"
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from "react-router-dom"
+
+const { Title } = Typography
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className="heading">
+        Global Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5"/>
+        </Col>
+      </Row>
+    </>
   )
 }
 
