e3724fb2619f16edfabf28058b3b58644f35ff87
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 15:42:45 -0400

    added setNewsCategory to our Select onChange, clicking on a crypto will search for news articles just for that crypto

--- cryptoapp/src/components/News.js
+++ cryptoapp/src/components/News.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react'
 import { Select, Typography, Row, Col, Avatar, Card} from "antd"
 import moment from "moment"
 import { useGetCryptoNewsQuery } from '../services/cryptoNews'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Text, Title} = Typography
 const { Option } = Select
 
 const News = ({simplified}) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 5 : 12})
-  const [readMore, setReadMore] = useState(false)
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency")
+
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 5 : 12})
+  const { data } = useGetCryptosQuery(100)
+
 
   if(!cryptoNews?.value) return "Loading..."
 
@@ -18,6 +22,21 @@ const News = ({simplified}) => {
     <>
      {!simplified && ( <h1 style={{"display": "flex", "justifyContent": "center", "alignItems": "center", "fontSize": '4rem'}}>News</h1>)}
       <Row gutter={[24, 24]}>
+        {!simplified && (
+          <Col span={24}>
+            <Select 
+            showSearch 
+            className='select-news' 
+            placeholder="Select Crypto" 
+            optionFilterProp='children' 
+            onChange={(value) => setNewsCategory(value)} 
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) > 0}>
+              <Option value="Cryptocurrency">Cryptocurrency</Option>
+              {/* we map through all of our coin data that we have from our useGetCryptoQuery */}
+              {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+            </Select>
+          </Col>
+        )}
         {/* need to map over the VALUES of cryptoNews to get our images and links and everything else important */}
         {cryptoNews.value.map((article, i) => (
           <Col xs={24} sm={12} lg={8} key={i}>
@@ -25,13 +44,16 @@ const News = ({simplified}) => {
               <a href={article.url} target="_blank" rel="noreferrer">
                 <div className='news-image-container'>
                   <Title className='news-title' level={4}>{article.name}</Title>
-                  <img src={article?.image?.thumbnail?.contentUrl || demoImage} alt="article" />
+                  <img style={{maxWidth: "200px", maxHeight: "100px", borderRadius: "4px"}} src={article?.image?.thumbnail?.contentUrl || demoImage} alt="article" />
                 </div>
                 <p>{article.description > 100 ? `${article.description.substring(0,150)}...` : article.description} </p>
                 <div className='provider-container'>
-                  <Avatar src={article.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" size={50}/> 
-                  <Text>{moment(article.datePublished).startOf("ss").fromNow()}</Text>
+                  <div>
+                    <Avatar src={article.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" size={50}/> 
+                    <Text className='provider-name'>{article.provider[0]?.name}</Text>
+                  </div>
                 </div>
+                  <Text>{moment(article.datePublished).startOf("ss").fromNow()}</Text>
               </a>
             </Card>
           </Col>
