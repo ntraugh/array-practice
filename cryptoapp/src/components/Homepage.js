81c4d4faa1e88ddb38d6b3c1d03c82122930e7f0
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 11:36:35 -0400

    fixed the Row/Col issues, each title was it's own Col with Span, not all wrapped in one Col

--- cryptoapp/src/components/Homepage.js
+++ cryptoapp/src/components/Homepage.js
@@ -12,9 +12,14 @@ const Homepage = () => {
         Global Stats
       </Title>
       <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5"/>
-        </Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+        <Col span={12}><Statistic title="Total 24hr Volume" value="5"/></Col>
+        <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+         
+          
+        
       </Row>
     </>
   )
