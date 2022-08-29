74862faf96975d2eacd18a4f2248259ed16a7b57
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 13:06:13 -0400

    mapped through our crypto coins and displayed a card for each one with the url, price, market cap, and daily change

--- cryptoapp/src/components/Cryptocurrencies.js
+++ cryptoapp/src/components/Cryptocurrencies.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import millify from "millify"
+import { Link } from "react-router-dom"
+import { Card, Row, Col, Input} from "antd"
+
+import { useGetCryptosQuery } from "../services/cryptoApi"
 
 const Cryptocurrencies = () => {
+
+  const { data: cryptosList, isFetching } = useGetCryptosQuery()
+
+  // setting our initial state value to the data we get back from the coins
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  console.log(cryptosList)
+
   return (
-    <div>Cryptocurrencies</div>
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((crypto) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+            <Link to={`/crypto/${crypto.id}`}>
+              <Card title={`${crypto.rank}. ${crypto.name}`} extra={<img className='crypto-image' src={crypto.iconUrl} />} hoverable>
+                <p>Price: {millify(crypto.price)}</p>
+                <p>Price: {millify(crypto.marketCap)}</p>
+                <p>Price: {millify(crypto.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
--- cryptoapp/src/components/Homepage.js
+++ cryptoapp/src/components/Homepage.js
@@ -3,13 +3,14 @@ import millify from "millify"
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from "react-router-dom"
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import {Cryptocurrencies, News} from "../components"
+
 
 const { Title } = Typography
 
 const Homepage = () => {
 
   const { data, isFetching } = useGetCryptosQuery()
-  console.log(data)
   
   // destructured stats from our API call (data.data). we could also grab "coins" from this call as well
   const stats = data?.data?.stats
@@ -34,6 +35,12 @@ const Homepage = () => {
         <Title level={2} className="home-title">Top 10</Title>
         <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
       </div>
+      <Cryptocurrencies simplified/>
+      <div className='home-heading-container'>
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified/>
     </>
   )
 }
