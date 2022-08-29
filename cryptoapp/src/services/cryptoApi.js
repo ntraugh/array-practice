fa201db2db802f1dc2db2c3df94481ba685a9951
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 11:44:03 -0400

    pasted the options from the RapidApi documentation into our cryptoApi file, imported configureStore for Redux into our store.js file

---
+++ cryptoapp/src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'd4a013123emsh1df389a29728209p1c3e74jsn82106f713090',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
