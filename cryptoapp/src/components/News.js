2c56432f68411210d2c04642a172d9066bcaef7d
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 14:10:51 -0400

    mapped over the cryptoNews data and displayed the first 5 articles on the home page, will show 12 on the news page

--- cryptoapp/src/components/News.js
+++ cryptoapp/src/components/News.js
@@ -7,10 +7,25 @@ const { Text, Title} = Typography
 const { Option } = Select
 
 const News = ({simplified}) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 10 : 100})
-  console.log(cryptoNews)
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 5 : 12})
+  
+  if(!cryptoNews?.value) return "Loading..."
+
   return (
-    <div>News</div>
+    <Row gutter={[24, 24]}>
+      {/* need to map over the VALUES of cryptoNews to get our images and links and everything else important */}
+      {cryptoNews.value.map((article, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={article.url} target="_blank" rel="noreferrer">
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>{article.name}</Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
