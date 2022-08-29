bbb4feb89195155ed942130a1dde95cf30700882
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 14:16:41 -0400

    set up ternary operator to deal with the length of the article, if it's more than 100 characters we add a ... after it to signal there is more content, if it's less we show the entire description

--- cryptoapp/src/components/News.js
+++ cryptoapp/src/components/News.js
@@ -11,6 +11,8 @@ const News = ({simplified}) => {
   
   if(!cryptoNews?.value) return "Loading..."
 
+  const demoImage = "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News"
+
   return (
     <Row gutter={[24, 24]}>
       {/* need to map over the VALUES of cryptoNews to get our images and links and everything else important */}
@@ -20,7 +22,9 @@ const News = ({simplified}) => {
             <a href={article.url} target="_blank" rel="noreferrer">
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{article.name}</Title>
+                <img src={article?.image?.thumbnail?.contentUrl || demoImage} alt="article" />
               </div>
+              <p>{article.description > 100 ? `${article.description.substring(0,100)}...` : article.description}</p>
             </a>
           </Card>
         </Col>
