b07eb72f988d3dbd6752404f868ea5d0347f8fb5
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 11:08:29 -0400

    created initial components and added them into our App.js under the Layout component from ant design

--- cryptoapp/src/components/index.js
+++ cryptoapp/src/components/index.js
@@ -1,1 +1,6 @@
+// if we have a lot of components we can use the index to export all of them to the App.js
+// that way we do it in less lines of code in the App
+// import { Navbar, CryptoList, Footer, CryptoMenu } from "./components" for example
+
+
 export { default as Navbar } from "./Navbar"
