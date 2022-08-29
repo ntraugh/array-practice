06d9e6e1f69c3c88551fe73335ac6ea90dbae5c9
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 12:25:20 -0400

    tried to switch to v2 of API but was getting CORS error needed upgraded plan, created globalStats variable that destructures our data to give back the stats object.  We grab those stats values within our values in our titles

--- cryptoapp/src/components/Homepage.js
+++ cryptoapp/src/components/Homepage.js
@@ -9,20 +9,25 @@ const { Title } = Typography
 const Homepage = () => {
 
   const { data, isFetching } = useGetCryptosQuery()
-
-  console.log(data)
   
+  // grabbing our data from the API calls.  we get data back from API calls then we go into the data object then get stats from there.
+  const globalStats = data?.data?.stats
+
+
+  if(isFetching) return "Loading..."
+
   return (
     <>
       <Title level={2} className="heading">
         Global Stats
       </Title>
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-        <Col span={12}><Statistic title="Total 24hr Volume" value="5"/></Col>
-        <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+        {/* because globalStats points to the stats object in our API we can just grab the total from it.  or total24hVolume, totalCoins,totalExchanges, etc */}
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges}/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={globalStats.totalMarketCap}/></Col>
+        <Col span={12}><Statistic title="Total 24hr Volume" value={globalStats.total24hVolume}/></Col>
+        <Col span={12}><Statistic title="Total Markets" value={globalStats.totalMarkets}/></Col>
          
           
         
