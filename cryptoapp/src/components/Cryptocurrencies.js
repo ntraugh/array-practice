ef9b92188d410895fb5226a966358c2ae3842956
Nate Traugh <n.traugh@yahoo.com>
2022-08-29 13:23:43 -0400

    set up our useEffect to run everytime our searchTerm or cryptosList is updated, using filter to match the searchTerm with the coin.name that matches

--- cryptoapp/src/components/Cryptocurrencies.js
+++ cryptoapp/src/components/Cryptocurrencies.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import millify from "millify"
 import { Link } from "react-router-dom"
 import { Card, Row, Col, Input} from "antd"
@@ -13,11 +13,25 @@ const Cryptocurrencies = ({simplified}) => {
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
 
   // setting our initial state value to the data we get back from the coins
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  const [cryptos, setCryptos] = useState([])
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+  // anytime our cryptosList or searchTerm changes we will run this useEffect
+  useEffect(() => {
+
+    // filtering our our data where the searchTerm includes the coin name. use exact match by making both terms lowercase()
+    const filtered = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setCryptos(filtered)
+  }, [cryptosList, searchTerm])
 
   if(isFetching) return "Loading..."
   return (
     <>
+      <div className='search-crypto'>
+        <Input placeholder='Search Cryptos' onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((crypto) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
