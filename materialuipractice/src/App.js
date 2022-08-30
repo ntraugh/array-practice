d3f0eac2206e7923dd706fc7ba4abd475c7ba740
Nate Traugh <n.traugh@yahoo.com>
2022-08-30 15:07:04 -0400

    messing around with Typography variants and ButtonGroups

--- materialuipractice/src/App.js
+++ materialuipractice/src/App.js
@@ -12,6 +12,9 @@ import {
   Container } from "@mui/material"
 
 import { PhotoCamera } from "@mui/icons-material"
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 
 function App() {
   return (
@@ -25,6 +28,23 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color="textPrimary" gutterBottom>
+              Photo Albums
+            </Typography>
+            <Typography variant='subtitle1' align="center" color="textSecondary" paragraph>
+              More Stuff to display on this page probably photo albums so you can click on them 
+            </Typography>
+          </Container>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button>One</Button>
+            <Button>Two</Button>
+            <Button>Three</Button>
+          </ButtonGroup>
+        </div>
+      </main>
     </>
   );
 }
