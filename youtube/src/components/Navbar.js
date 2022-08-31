2136587ac9ab138178e55c0fcf6e77efa7b6d50f
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 12:38:26 -0400

    setup the initial search bar style by using Paper and IconButton components, as well as the Search icon from Mui, styled inline with sx property

--- youtube/src/components/Navbar.js
+++ youtube/src/components/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
 // hover over each component and click demo to get a demo
 import { Stack } from "@mui/material"
+import { Link } from "react-router-dom"
+import { logo } from "../utils/constants"
+import Searchbar from './Searchbar'
 
 const Navbar = () => {
   return (
-    <Stack spacing={2} direction="row">
-      <ul>
-        <li>
-          Stuff
-        </li>
-        <li>
-          Stuff
-        </li>
-        <li>
-          Stuff
-        </li>
-      </ul>
+    <Stack 
+      spacing={2} 
+      direction="row" 
+      alignItems="center" 
+      p={2} 
+      sx={{position: "sticky", background: "#000", top: 0, justifyContent: "space-between"}}>
+      <Link to="/" style={{display: "flex", alignItems: "center"}}>
+        {/* image is initially huge, place a height property on it to change the size */}
+        <img src={logo} alt="ourlogo" height={45} />
+      </Link>
+      <Searchbar />
     </Stack>
   )
 }
