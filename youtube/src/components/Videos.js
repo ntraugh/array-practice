25717478dcbc38e3b468bdd8efa449a2a9fa4d85
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 15:09:06 -0400

    styled the ChannelCard to take up the correct space, image for channel is not coming through though

--- youtube/src/components/Videos.js
+++ youtube/src/components/Videos.js
@@ -10,7 +10,7 @@ const Videos = ({videos}) => {
         return <Box key={i}>
 {/* if there is a videoId on the item we render a VideoCard component */}
           {video.id.videoId && <VideoCard video={video}/>}
-          {/* {video.id.channelId && <ChannelCard channel={video}/>} */}
+          {video.id.channelId && <ChannelCard channel={video}/>}
         </Box>
       })}
     </Stack>
