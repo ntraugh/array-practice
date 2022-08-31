91a50e8f445b40d9fe7f640f56c0db369db8b1ae
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 12:10:12 -0400

    created constants file in utils folder that consists of our categories, and icons/logo

--- youtube/src/components/Navbar.js
+++ youtube/src/components/Navbar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+// hover over each component and click demo to get a demo
+import { Stack } from "@mui/material"
 
 const Navbar = () => {
   return (
-    <div style={{color: "white"}}>Navbar</div>
+    <Stack spacing={2} direction="row">
+      <ul>
+        <li>
+          Stuff
+        </li>
+        <li>
+          Stuff
+        </li>
+        <li>
+          Stuff
+        </li>
+      </ul>
+    </Stack>
   )
 }
 
