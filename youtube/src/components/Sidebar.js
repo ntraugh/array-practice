6475d5d1019b5350c16c432a2da6b25787a8cca3
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 12:57:51 -0400

    mapped through the categories to place the icons and names of categories on the sidebar, used inline styling to set the backgrounds, hardcoded the background color for now will be dynamic in the future

--- youtube/src/components/Sidebar.js
+++ youtube/src/components/Sidebar.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Stack } from "@mui/material"
+
+import { categories } from '../utils/constants'
+const selectedCat = "New"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+// create a Stack with direction of Row for the sidebar
+    <Stack direction="row" sx={{ 
+        overflowY: "auto", 
+        height: { sx: "auto", md: "95%"},
+        flexDirection: { md: "column"}}}>
+{/* map through our categories and get the name and icon to display a button */}
+        {categories.map((cat, i) => {
+            return <button 
+            key={i} 
+            className="category-btn" 
+            style={
+                {background: cat.name === selectedCat && "#FC1503",
+                color: "white"}}>
+               <span >
+                    {cat.name}
+                </span>
+               <span style={
+                    { color: cat.name === selectedCat ? "white": "red"}}>
+                    {cat.icon}
+                </span>
+            </button>
+        })}
+    </Stack>
   )
 }
 
