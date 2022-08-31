eb013e2b216e5172da48c291e9157ac7050c4318
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 12:41:28 -0400

    created initial sidebar layout with the footer

--- youtube/src/components/Searchbar.js
+++ youtube/src/components/Searchbar.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom"
-import { Paper, Iconbutton, IconButton} from "@mui/material"
+import { Paper, IconButton} from "@mui/material"
 import { Search } from "@mui/icons-material"
 
 const Searchbar = () => {
   return (
-    <Paper component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl:2, boxShadow: "none", mr: { sm: 5 }}}>
-        <input className='search-bar' placeholder='Search' value="" onChange={() => {}} />
+    <Paper elevation={3} component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl:2, boxShadow: "none", mr: { sm: 5 }}}>
+        <input 
+            className='search-bar' 
+            placeholder='Search' 
+            value="" 
+            onChange={() => {}} 
+        />
         <IconButton type='submit' sx={{ p: "10px", color: "red"}}>
             <Search />
         </IconButton>
-
     </Paper>
   )
 }
