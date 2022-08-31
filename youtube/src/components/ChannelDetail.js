e132a946dd77a33f418e06af6800f62865a2c060
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 11:53:37 -0400

    first set up our routes in the App.js with browserRouter, then set up each component that we named as an element in our BrowserRouter Routes, created index.js file for all of our components so we don't import everything line by line into the App.js

---
+++ youtube/src/components/ChannelDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChannelDetail = () => {
+  return (
+    <div>Channel</div>
+  )
+}
+
+export default ChannelDetail
---
+++ youtube/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div style={{color: "white"}}>Navbar</div>
+  )
+}
+
+export default Navbar
