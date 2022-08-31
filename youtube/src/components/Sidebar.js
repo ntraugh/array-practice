159bcec144a6a3c101fc69569a80ff09a13011ba
Nate Traugh <n.traugh@yahoo.com>
2022-08-31 13:29:08 -0400

    set up our environment variables and our base request in fetchApi file

--- youtube/src/components/Sidebar.js
+++ youtube/src/components/Sidebar.js
@@ -11,6 +11,7 @@ const Sidebar = () => {
     <Stack direction="row" sx={{ 
         overflowY: "auto", 
         height: { sx: "auto", md: "95%"},
+        // on medium and higher screens we go to a column view
         flexDirection: { md: "column"}}}>
 {/* map through our categories and get the name and icon to display a button */}
         {categories.map((cat, i) => {
