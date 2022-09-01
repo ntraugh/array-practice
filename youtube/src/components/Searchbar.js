6800a087baf64f135e32cab57d124f31902f7249
Nate Traugh <n.traugh@yahoo.com>
2022-09-01 16:46:37 -0400

    set up search functionality by implementing our onChange function setSearchTerm to e.target.value, created our handleSubmit that checks if the searchTerm is present and if it is we use the useNavigate hook to navigate the user to that specific searchTerm

--- youtube/src/components/Searchbar.js
+++ youtube/src/components/Searchbar.js
@@ -4,13 +4,30 @@ import { Paper, IconButton} from "@mui/material"
 import { Search } from "@mui/icons-material"
 
 const Searchbar = () => {
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+
+      setSearchTerm("")
+    }
+  }
+
+  const [searchTerm, setSearchTerm] = useState("")
   return (
-    <Paper elevation={3} component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl:2, boxShadow: "none", mr: { sm: 5 }}}>
+    <Paper 
+      elevation={3} 
+      component="form" 
+      onSubmit={handleSubmit} 
+      sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl:2, boxShadow: "none", mr: { sm: 5 }}}>
         <input 
             className='search-bar' 
             placeholder='Search' 
-            value="" 
-            onChange={() => {}} 
+            value={searchTerm} 
+            onChange={(e) => setSearchTerm(e.target.value)} 
         />
         <IconButton type='submit' sx={{ p: "10px", color: "red"}}>
             <Search />
