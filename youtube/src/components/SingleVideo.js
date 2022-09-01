8be0ae4dc80d3073b46fbc9ffde3ae83949ea7da
Nate Traugh <n.traugh@yahoo.com>
2022-09-01 17:10:40 -0400

    fixed typo and video now takes up full width of the screen like it's supposed to

--- youtube/src/components/SingleVideo.js
+++ youtube/src/components/SingleVideo.js
@@ -19,14 +19,43 @@ const SingleVideo = () => {
     .then((data) => setDetail(data.items[0]))
   }, [id])
 
+  if(!detail?.snippet) return "Loading..."
+
+  const { snippet: {title, channelId, channelTitle}, statistics: { viewCount, likeCount} } = detail
+
   return (
 
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row"}}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px"}}>
-            {/* reacPlayer takes in a url */}
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
+            {/* reacPlayer takes in a url from a video sharing site */}
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff"}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography 
+                  variant={{ sm: "subtitle1", md: "h6"}} color="#fff">
+                    {channelTitle}
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography
+                  variant="body1"
+                  sx={{opacity: 0.7}}>
+                    {parseInt(viewCount).toLocaleString()}
+                    views
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{opacity: 0.7}}>
+                    {parseInt(likeCount).toLocaleString()}
+                    likes
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
         </Box>
       </Stack>
