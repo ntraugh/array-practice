478744542072afaa27e11df62409369eece9f783
Nate Traugh <n.traugh@yahoo.com>
2022-09-05 13:53:06 -0400

    set up initial navbar with image that needs styling, imported our Posts and Form components into the App.js

--- socialmedia/client/src/App.js
+++ socialmedia/client/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid} from "@mui/material"
+import memories from "./components/images/memories.png"
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="600" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={2}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
